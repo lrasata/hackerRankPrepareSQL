044f1fa6312e5d0aa4f253ad539a1de7234252e7
lrasata <rmliantsoa@gmail.com>
2021-12-25 22:31:56 +1300

    Prep sql basic medium Submitted successfully

--- prepareSQLBasicMedium.sql
+++ prepareSQLBasicMedium.sql
@@ -30,3 +30,31 @@ from
     group by occupation
     order by count(*) asc) as t
 ;
+
+/*
+	Author : lrasata
+	HackerRankChallenge - Weather Observation Station 18
+    Query the Manhattan Distance between points P1 and P2 and round it to a scale of  decimal places.
+*/
+
+select 
+    format(
+        abs(min(lat_n) - max(lat_n)) + 
+        abs(min(long_w) -  max(long_w))
+        , 4) 
+from station;
+
+/*
+	Author : lrasata
+	HackerRankChallenge - Weather Observation Station 19
+    Query the Euclidean Distance between points P1 and P2 and format your answer to display  decimal digits.
+*/
+
+select 
+    format(
+        sqrt(
+            power((max(lat_n) - min(lat_n)), 2)  + 
+            power((max(long_w) - min(long_w)), 2)
+        ), 4
+    ) 
+from station;
